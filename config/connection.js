e76b76260fa1d5021ec8ebffc3f84e1f68560ad3
AlkaTone <antonio.a.garces@gmail.com>
2020-06-07 15:51:15 -0700

    deployed to heroku & incorporated database

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,11 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection(
   process.env.JAWSDB_URL || {
-  host: "localhost",
+  host: "sq65ur5a5bj7flas.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "Monopoly12",
-  database: "burger_db"
+  user: "m0ne5sp978ttahpa",
+  password: "ivkwflmgzngbxzy4",
+  database: "gj1df32ew8x4yr3u"
   });
 
 // Make connection.
