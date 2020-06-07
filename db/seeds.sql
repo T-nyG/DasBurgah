b528ecd674a64176109d0edbc32b465a4b7d1052
AlkaTone <antonio.a.garces@gmail.com>
2020-06-07 15:38:24 -0700

    full app done

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burgerName) VALUES ('Burger Meister');
+INSERT INTO burgers (burgerName) VALUES ('Butter Bronson');
+INSERT INTO burgers (burgerName) VALUES ('Fräulein Burgah');
+INSERT INTO burgers (burgerName) VALUES ('Am Flutgraben');
+INSERT INTO burgers (burgerName) VALUES ('Mogg And Melzer');
+INSERT INTO burgers (burgerName, devoured) VALUES ('Shiso Burger', true);
